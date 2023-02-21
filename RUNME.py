5cc99249aeb6ae17add72edc7c4afb59694423a2
dbbnicole <86326159+dbbnicole@users.noreply.github.com>
2023-02-21 17:53:58 -0600

    Update RUNME.py

--- RUNME.py
+++ RUNME.py
@@ -64,7 +64,8 @@ job_json = {
         "max_concurrent_runs": 1,
         "tags": {
             "usage": "solacc_testing",
-            "group": "RCG"
+            "group": "RCG",
+            "accelerator": "wide-and-deep"
         },
         "tasks": [
             {
@@ -112,7 +113,9 @@ job_json = {
                     "num_workers": 16,
                     "node_type_id": {"AWS": "i3.xlarge", "MSA": "Standard_DS3_v2", "GCP": "n1-highmem-4"},
                     "custom_tags": {
-                        "usage": "solacc_testing"
+                        "usage": "solacc_testing",
+                        "group": "RCG",
+                        "accelerator": "wide-and-deep"
                     },
                 }
             }
